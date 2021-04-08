cbabe5de48a0b48c2af60ecf117541ede10e24b3
Luu Nguyen <nxuluu@gmail.com>
2021-04-08 08:53:26 +0700

    Asset file with some image

---
+++ src/asset/item.js
@@ -0,0 +1,43 @@
+﻿const item = {
+  "to": 1544342399,
+  "from": 1543737600,
+  "entries": [
+    {
+      "dayOfWeek": 0,
+      "Income": 1000,
+      "Expense": 30
+    },
+    {
+      "dayOfWeek": 1,
+      "Income": 425.75,
+      "Expense": 81.25
+    },
+    {
+      "dayOfWeek": 2,
+      "Income": 59.97,
+      "Expense": 102.72
+    },
+    {
+      "dayOfWeek": 3,
+      "Income": 29,
+      "Expense": 88
+    },
+    {
+      "dayOfWeek": 4,
+      "Income": 288,
+      "Expense": 0
+    },
+    {
+      "dayOfWeek": 5,
+      "Income": 128,
+      "Expense": 0
+    },
+    {
+      "dayOfWeek": 6,
+      "Income": 24,
+      "Expense": 534
+    }
+  ],
+};
+
+export default item;
